ff40970dbb3b8fe1308f07d6eea2b04a7380b608
IgorSousaFront <igor.ssqwe@gmail.com>
2017-05-07 11:37:33 -0300

    área de serviços finalizados

--- public/index.php
+++ public/index.php
@@ -47,12 +47,98 @@
 				</div>
 			</div>
 		</header>
+<section id="service">
+    <div class="container">
+        <header class="row">
+            <div class="col-xs-12">
+                <h3 class="title-section">Serviços</h3>
+            </div>
+        </header>
+        <div class="row list-service text-center">
+            <div class="col-xs-12 col-sm-6 col-md-3">
+                <div class="service-item">
+                    <div class="bloco-img">
+                        <img class="img-responsive" src="img/services-icon01.png">
+                    </div>
+                    <h3>GESTÃO DE REDES SOCIAIS</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur.</p>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-3">
+                <div class="service-item">
+                    <div class="bloco-img">
+                        <img class="img-responsive" src="img/services-icon02.png">
+                    </div>
+                    <h3>MARKETING DE CONTEÚDO</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur.</p>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-3">
+                <div class="service-item">
+                    <div class="bloco-img">
+                        <img class="img-responsive" src="img/services-icon03.png">
+                    </div>
+                    <h3>SOLUÇOES PARA A INTERNET</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur.</p>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-3">
+                <div class="service-item">
+                    <div class="bloco-img">
+                        <img class="img-responsive" src="img/services-icon04.png">
+                    </div>
+                    <h3>E-MAIL MARKETING</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-6 background-mkt">
+
+        </div>
+        <div class="col-md-6 background-dev">
+
+        </div>
+    </div>
+
+
+</section>
+
+
+<section id="portifolio">
+    <div class="container">
+
+        <div class="row cabecalho-portifolio text-center">
+            <h3 class="title">NOSSO POSTIFOLIO</h3>
+            <p class="sub-title">Conheça nosso portfólio</p>
+            <button class="btn active">Todos</button>
+            <button class="btn">Sites</button>
+            <button class="btn">Social Midia</button>
+        </div>
+
+        <div class="row img-portifolio">
+            <div class="col-lg-4 col-sm-6"><img class="img-responsive img-thumbnail " src="img/1.png"></div>
+            <div class="col-lg-4 col-sm-6"><img class="img-responsive img-thumbnail" src="img/2.png"></div>
+            <div class="col-lg-4 col-sm-6"><img class="img-responsive img-thumbnail" src="img/3.png"></div>
+            <div class="col-lg-4 col-sm-6"><img class="img-responsive img-thumbnail" src="img/4.png"></div>
+            <div class="col-lg-4 col-sm-6 "><img class="img-responsive img-thumbnail" src="img/5.png"></div>
+            <div class="col-lg-4 col-sm-6"><img class="img-responsive img-thumbnail" src="img/6.png"></div>
+        </div>
+    </div>
+</section>
+
+
+<section id="modelos" style="height: 400px;background-color: #ececec;">
+</section>
+
 
 <section id="contato" class="container">
     <div class="row">
 
         <div class="col-md-6">
-
+            img
         </div>
 
         <div class="col-md-6 col-xs-12">
@@ -75,10 +161,13 @@
                 <div class="col-md-12 col-xs-12">
                     <textarea class="form-control form-group input-lg" rows="4" placeholder="DIigite sua mensagem"></textarea>
                 </div>
-             </div>
+            </div>
         </div>
     </div>
 </section>
+
+
+
 
 <section id="diagnostico">
     <div class="container">
@@ -87,13 +176,13 @@
                 <h3 class="title">DIAGNÓSTICO GRÁTIS</h3>
                 <p>Avalie suas ações de marketing digital e saiba
                     quais seus próximos passoas para o sucesso.</p>
-            </div>
-            <div class="col-md-6 text-right asd" style="vertical-align: center">
-                <button class="btn-primary">SOLICITAR DIAGNÓSTICO GRATUITO</button>
+                </div>
+                <div class="col-md-6 btn-">
+                    <button class="btn-primary">SOLICITAR DIAGNÓSTICO GRATUITO</button>
+                </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 <footer id="footer">
     <div class="footer-top">
         <div class="container">
